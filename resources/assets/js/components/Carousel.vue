a9923e1ad3585c7068793d6200a5e7d2ca20c2ad
Michael Joyner <joyner.michael@gmail.com>
2016-10-18 21:39:54 +0800

    go back to canplaythrough event

--- resources/assets/js/components/Carousel.vue
+++ resources/assets/js/components/Carousel.vue
@@ -10,7 +10,7 @@
         >
             <div class="media-aspect-box">
                 <img @load="markAsReady(slide, $event)" :src="slide.image_src" :alt="slide.slide_text" v-if="!slide.is_video">
-                <video @loadeddata="markAsReady(slide)" :src="'/videos/' + slide.video" autoplay muted loop v-if="slide.is_video"></video>
+                <video @canplaythrough="markAsReady(slide)" :src="'/videos/' + slide.video" autoplay muted loop v-if="slide.is_video"></video>
             </div>
             <span class="slide-text">{{ slide.slide_text }}</span>
             <a v-if="slide.action_link && slide.action_text" href="{{ slide.action_link }}" class="slide-action">{{
