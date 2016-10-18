f438e6d6abe6cd482a03775e06a4e18c3107d794
Michael Joyner <joyner.michael@gmail.com>
2016-10-18 23:55:48 +0800

    add playsinline to video tag

--- resources/views/front/home/partials/carousel.blade.php
+++ resources/views/front/home/partials/carousel.blade.php
@@ -3,7 +3,7 @@
     <div class="slide carousel-slide original-slide {{ $slide->text_colour }}">
         <div class="media-aspect-box">
             @if($slide->is_video)
-                <video src="/videos/{{ $slide->video }}" autoplay muted loop></video>
+                <video src="/videos/{{ $slide->video }}" autoplay playsinline muted loop></video>
             @else
                 <img src="{{ $slide->modelImage('large') }}" alt="{{ $slide->slide_text }}">
             @endif
