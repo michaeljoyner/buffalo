cf627d7391c634b63679a40325fbe107d88b6efe
Michael Joyner <joyner.michael@gmail.com>
2016-10-18 09:14:12 +0800

    copy changes

--- resources/views/front/contact/page.blade.php
+++ resources/views/front/contact/page.blade.php
@@ -6,12 +6,12 @@
 
 @section('content')
     <section class="page-banner contact-page-banner">
-        <h1 class="h1 banner-quote to-right text-white">Reach out and touch me</h1>
+        <h1 class="h1 banner-quote to-left">Let's connect.</h1>
     </section>
     <section class="page-section">
         <h1 class="h1 section-title">Contact Us</h1>
         <p class="strong-lead">
-            Please don't hesitate to contact us with any query or question you may have. We would love to get in touch and se how we can work with you.
+            Please don't hesitate to contact us with any query or question you may have. We would love to get in touch and see how we can work with you.
         </p>
     </section>
     <section class="page-section contact-section full-contact-page-section">
