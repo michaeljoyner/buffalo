114d4e7afffd6247b7a55ec6839345c264665544
Michael Joyner <joyner.michael@gmail.com>
2017-01-31 12:00:06 +0800

    use versioned admin js file

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -20,7 +20,7 @@
 <div class="main-footer"></div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script src="/js/admin.js"></script>
+<script src="{{ elixir('js/admin.js') }}"></script>
 @include('admin.partials.flash')
 @yield('bodyscripts')
 </body>
