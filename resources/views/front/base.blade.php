9d25809cbe4d34ed9c836540197ab05bcbcf6796
Michael Joyner <joyner.michael@gmail.com>
2017-05-01 10:11:07 +0800

    remove empty description tag from base view

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>@yield('title', 'Buffalo Tools')</title>
-    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
     <link rel="stylesheet" href="{{ elixir('css/fapp.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:700|Teko:700|Ubuntu" rel="stylesheet">
